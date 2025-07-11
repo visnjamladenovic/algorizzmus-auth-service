a4c350c201839bb4fa1b803d29dfcf2775ac3c5a
Visnja Mladenovic <visnjagithub@gmail.com>
2025-07-11 19:42:20 +0200

    Fix build

--- src/main/kotlin/com/vim/algorizzmusauthservice/application/security/JwtGenerator.kt
+++ src/main/kotlin/com/vim/algorizzmusauthservice/application/security/JwtGenerator.kt
@@ -3,9 +3,6 @@ package com.vim.algorizzmusauthservice.application.security
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
 import java.util.Date
@@ -27,11 +24,6 @@ class JwtGenerator {
             .setExpiration(Date(System.currentTimeMillis() + EXPIRATION))
             .signWith(key)
             .compact()
-    }
-
-    @Bean
-    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
-        return authenticationConfiguration.authenticationManager
     }
 
     companion object {
